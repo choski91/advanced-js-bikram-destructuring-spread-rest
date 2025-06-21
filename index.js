6539990e1f286e0d6e98711d4c0a62bfed659720
choski91 <mlaura.smichowski@gmail.com>
2025-06-21 12:56:55 +0200

    hasta el ejercicio 2

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 //RESUELVE LOS EJERCICIOS AQUÍ
+//ejercicio 1
+
+const empleados = [
+    { name: "Luis", email: "Luis@gmail.com" },
+    { name: "Ana", email: "Ana@gmail.com" },
+    { name: "Andrea", email: "Andrea@gmail.com" },
+];
+
+//const {name, email} = empleados;
+
+const ana = empleados [1];
+
+console.log(ana);
+
+//ejercicio 2
+
+const Luis = empleados [0];
+
+const {name, email } = Luis;
+
+const emailLuis = email;
+
+console.log(emailLuis);
+
+//ejercicio 3
